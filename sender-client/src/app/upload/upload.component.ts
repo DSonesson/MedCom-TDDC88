33384128b677b96b52317867b229bfca6fe3e577
swe4lbin <swe4lbin@hotmail.com>
2019-10-08 16:10:03 +0200

    Image upload preview functionality
    Upload image works using a div instead of the input file, which is hidden with css.
    
    Co-Authored-By: henrikliu <henrikliu@users.noreply.github.com>

--- sender-client/src/app/upload/upload.component.ts
+++ sender-client/src/app/upload/upload.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent implements OnInit {
-
+  public imagePath;
+  imgURL: any;
+  public message: string;
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  preview(files) {
+    if (files.length === 0)
+      return;
+ 
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+ 
+    var reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]); 
+    reader.onload = (_event) => { 
+      this.imgURL = reader.result; 
+    }
+  }
+  myEventHandler(){
+    console.log("Hello1");
+    let element: HTMLElement = document.getElementsByClassName('test')[0] as HTMLElement;
+    element.click();
+    }
+
+  onSubmit() {
   }
 
 }
