5ef35cebef91e26b615608d675f19ffc1c747729
swe4lbin <swe4lbin@hotmail.com>
2019-10-08 13:40:46 +0200

    Created upload component
    A component handling upload of images

--- sender-client/src/app/app.module.ts
+++ sender-client/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule
