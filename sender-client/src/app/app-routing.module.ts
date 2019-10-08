e146a585cc86513c20b088198d677a3b356c409d
Henrikliu <36007571+Henrikliu@users.noreply.github.com>
2019-10-08 14:51:06 +0200

    first attempt at routing
    attempt at routing, currently not working
    
    Co-Authored-By: swe4lbin <swe4lbin@users.noreply.github.com>

---
+++ sender-client/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { UploadComponent } from './upload/upload.component';
+
+
+const routes: Routes = [
+  { path: 'upload', component: UploadComponent }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+
+export class AppRoutingModule { }
--- sender-client/src/app/app.module.ts
+++ sender-client/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UploadComponent } from './upload/upload.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UploadComponent
+    UploadComponent,
+    ConfirmationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
