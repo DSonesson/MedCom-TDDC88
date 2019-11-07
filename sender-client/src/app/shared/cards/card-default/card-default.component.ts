9e5f31d99232fc9624d229454da02696e21c310e
Oscar Baart <baart.oscar@gmail.com>
2019-11-07 21:57:02 +0100

    Added name, email and number to summary

--- sender-client/src/app/shared/cards/card-default/card-default.component.ts
+++ sender-client/src/app/shared/cards/card-default/card-default.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CaseDataService } from '../../case-data.service';
+import { Case } from '../../../models/case';
 
 @Component({
   selector: 'app-card-default',
   templateUrl: './card-default.component.html',
   styleUrls: ['./card-default.component.scss']
 })
+
 export class CardDefaultComponent implements OnInit {
 
-  private case: case;
+  private case: Case;
+
 
   constructor(public dataService: CaseDataService) { 
     this.case = this.dataService.getCase();
