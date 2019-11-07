26d72e2485a411fc2f827db8f650bba6ed464c42
Henrikliu <36007571+Henrikliu@users.noreply.github.com>
2019-11-07 13:49:17 +0100

    SC-33 setting up

--- sender-client/src/app/layouts/content/content.routing.ts
+++ sender-client/src/app/layouts/content/content.routing.ts
@@ -20,7 +20,7 @@ export const ContentRoutes: Routes = [
     
  /* Routing Specification, place all "pages" that will be created here under the right alias and path */    
  { 
-    path: '', redirectTo: 'frontpage',pathMatch: 'full',
+    path: '', redirectTo: 'page2',pathMatch: 'full',
  },
     { path: 'frontpage', component: FrontpageComponent }, 
     { path: 'summary', component: SummaryComponent },
