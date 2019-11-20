82305fceac2f42a007497641724232f2d19328b3
Gustav Wahlquist <guswa204@student.liu.se>
2019-11-20 13:54:25 +0100

    added unit test for getCaseNr()

--- sender-client/src/app/shared/case-nr.service.spec.ts
+++ sender-client/src/app/shared/case-nr.service.spec.ts
@@ -9,4 +9,14 @@ describe('CaseNrService', () => {
     const service: CaseNrService = TestBed.get(CaseNrService);
     expect(service).toBeTruthy();
   });
+
+  it('should return caseNr with correct length', () => {
+    const service: CaseNrService = TestBed.get(CaseNrService);
+    let caseNr;
+    caseNr = service.getCaseNr("0701234567")
+    expect(caseNr.length == 8).toBeTruthy();
+  });
+
+
 });
+
