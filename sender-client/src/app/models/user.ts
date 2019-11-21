b06fbcd2848f855d822cd8ae81dc64bc03f5b179
Daniel Sonesson <dsonesson@c-10-253-241-138.clients.eduroam.liu.se>
2019-11-21 13:20:45 +0100

    Made mock-data add empty strings

--- sender-client/src/app/models/user.ts
+++ sender-client/src/app/models/user.ts
@@ -2,16 +2,15 @@ export class User {
     name: string;
     email: string;
     phone: string;
-    
+
     constructor() {
         this.dummyData();
 
     }
 
     dummyData() {
-        this.name = "Full Name";
-        this.email = "ullname@hotmail.com";
-        this.phone  = "123456789";
+        this.name = "";
+        this.email = "";
+        this.phone  = "";
     }
   }
-
