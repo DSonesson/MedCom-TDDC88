259778ab978067f44b0b800b33536c45e2f66126
Gustav Wahlquist <guswa204@student.liu.se>
2019-11-27 10:06:40 +0100

    Added code comments for the file: case-nr.service.ts

--- sender-client/src/app/shared/case-nr.service.ts
+++ sender-client/src/app/shared/case-nr.service.ts
@@ -5,16 +5,26 @@ import { Injectable } from '@angular/core';
 })
 export class CaseNrService {
 
+  /**
+   * Creates a instance of CaseNrService.
+   */
   constructor() { }
 
+
+  /**
+   * Computes the case number from a telephone number and the current time stamp.
+   * @param teleNum A 10 digit telephone number to compute the case number for.
+   *
+   * @returns The computed 8 digit case number as a string.
+   */
   getCaseNr(teleNum:string):string {
     var dateNow = new Date();
     var timeStamp = String(dateNow.getTime()).substr(0,10)
     let id = Number(timeStamp.substr(2,12) +  teleNum.substr(4,6))
     let dict64:string[] = ['0','1','2','3','4','5','6','7','8','9',
-                'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-                'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-                '-','+']
+      'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+      'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+      '-','+']
     var uuid:string = ''
     let last = 0
 
