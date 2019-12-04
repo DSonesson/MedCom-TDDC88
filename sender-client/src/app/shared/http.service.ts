399cbe23a72f7c578260bda84b5b22e0319c6340
liliang <liliang@c-10-253-224-97.clients.eduroam.liu.se>
2019-12-04 11:31:37 +0100

    remove invalid code

--- sender-client/src/app/shared/http.service.ts
+++ sender-client/src/app/shared/http.service.ts
@@ -6,8 +6,6 @@ import { Observable } from 'rxjs';
 import { stringify } from 'querystring';
 import { NgxXml2jsonService } from 'ngx-xml2json';
 import xml2js from 'xml2js';
-// import * as token from '../../../../../token.json'
-import * as token from '../../app/token.json'
 
 
 @Injectable({
@@ -39,10 +37,6 @@ export class HttpService {
       'Response-Type': 'text',
       'Access-Control-Allow-Origin': '*',
       observe: 'response'
-      // 'Access-Control-Allow-Methods': 'PUT, GET, POST, DELETE, OPTIONS',
-      // 'Access-Control-Allow-Headers': 'Content-Type',
-      //'Cookie': 'X-XSRF-TOKEN=aqt1uuezrl4q9uu03ipl'
-      // "User-Agent":"Chrome/56.0.2924.87"
     })
     const params = {
       'userid': 'annro873', 'password': 'vLieZJzd'
@@ -136,27 +130,10 @@ export class HttpService {
   async doSearch(caseNr: string, searchloc: string): Promise<boolean> {
 
     const loginData = await this.userLogin1();
-    console.log(loginData);
-
+    
     const searchData = await this.search(caseNr, searchloc);
-    console.log(searchData);
-
-    // var parser = require('xml2js').parseString;
-    // const parser = new xml2js.Parser({ strict: false, trim: true });
+    
     var total = 0;
-    // parser.parseString(searchData, (err, result) => {
-    //   console.log(result);
-    //   total = 1;
-    // });
-    // parser(searchData, (err, result) => {
-    //   console.log(result);
-    //   total = 1;
-    // });
-
-    // const parser = new xml2js.Parser({
-    //     trim: true,
-    //     explicitArray: true
-    // });
 
     xml2js.parseString(searchData, function (err, result) {
       console.log(result.entries.meta[0].total[0]);
@@ -185,47 +162,7 @@ export class HttpService {
     return this.http.request('POST', req, { headers, params, 'responseType': "text" }).toPromise();
   }
 
-  // private parseXML(data) {
-  //   return new Promise(resolve => {
-  //     var k: string | number,
-  //       arr = [],
-  //       parser = new xml2js.Parser(
-  //         {
-  //           trim: true,
-  //           explicitArray: true
-  //         });
-  //     parser.parseString(data, function (err, result) {
-  //       var obj = result.Employee;
-  //       for (k in obj.emp) {
-  //         var item = obj.emp[k];
-  //         arr.push({
-  //           id: item.id[0],
-  //           name: item.name[0],
-  //           gender: item.gender[0],
-  //           mobile: item.mobile[0]
-  //         });
-  //       }
-  //       resolve(arr);
-  //     });
-  //   });
-  // }
-
-  getTokenTest() {
-    // const clientId = "regionostergotland.se-2019-11-briva"
-    // const grantType = "client_credentials"
-    // const clientSecret = "TjJO5eEDUCmTT6wSClz28btkrHmMSm3QargesbrzDqMwVySzvgY_lP81cSeq_qVI"
-    // this.getToken(clientId, grantType, clientSecret).then(data => {
-    //   console.log(data)
-    // })
-    // console.log(token)
-    this.getToken(token.clientId, token.grantType, token.clientSecret).then(data => {
-      console.log(data)
-    })
-  }
-
-  async getToken(clientId: string, grantType: string, clientSecret: string) : Promise<string> {
-    // const url = 'https://idp.lio.se';
-    // const url = 'http://localhost:4200/api1';  
+  async getToken() : Promise<string> {
     const req = '/api1/auth/token';
       const headers = new HttpHeaders({
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -234,40 +171,7 @@ export class HttpService {
         'Access-Control-Allow-Origin': '*'
       });
 
-      const params = {
-        'client_id': clientId,
-        'grant_type': grantType,
-        'client_secret': clientSecret
-      }
-  
-      // const rsp = await this.http.request('POST', req, {params}).toPromise();
-      // this.http.request('POST', req, {params}).subscribe(data => {
-      //   console.log(data);
-      // });
-
-      const payload = new HttpParams()
-      .set('client_id', clientId)
-      .set('grant_type', grantType)
-      .set('client_secret', clientSecret);
-      // const rsp = await this.http.post<TokenJson>(req, payload).toPromise<TokenJson>()
       const rsp = await this.http.get<TokenJson>(req).toPromise<TokenJson>()
-      // const rsp = await this.http.get<TokenJson>("http://localhost:8080/auth/token").toPromise<TokenJson>()
-
-      console.log(rsp);
-      console.log(rsp.accessToken)
-
-      // this.http.post<string>(req, payload).subscribe(data => {
-      //   const str = JSON.stringify(data);
-      //   const ret = JSON.parse(str);
-      //   console.log(ret.access_token)
-      // })
-      // const json = JSON.parse(rsp);
-      // console.log(json.token)
-      // var token: string = "";
-      // xml2js.parseString(rsp, function (err, result) {
-      //   console.log(err);
-      //   // token = result.access_token;
-      // });
       
       return rsp.accessToken;
   }
